d306d5a9718cbbd11cb56b03bf858e16161342a2
Thinh <tienthinh12102@gmail.com>
2021-05-30 21:08:24 +0700

    Edit files

---
+++ php/homepage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Thinh-sama</title>
+</head>
+<body>
+	<div align="center">
+		<img src="./images/678.gif" width="510px" height="750px">
+	</div>
+	
+</body>
+</html>
